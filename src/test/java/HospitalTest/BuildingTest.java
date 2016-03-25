b20deed53198068b52b1e2746377d2f0da522b25
mplemay <mplemay97@gmail.com>
2016-03-24 23:06:30 -0400

    modified travis and cradle

---
+++ src/test/java/HospitalTest/BuildingTest.java
@@ -0,0 +1,19 @@
+package HospitalTests;
+
+import Hospital.Building;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by matthewlemay on 3/21/16.
+ */
+
+public class BuildingTest {
+
+
+    @Test
+    public void helloTest() {
+        assertEquals("Hello", "Hello");
+    }
+
+}
