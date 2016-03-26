4d98149a530d2faf8f7b24239d8d31e5edc042eb
mplemay <mplemay97@gmail.com>
2016-03-26 17:07:27 -0400

    added graph stream

---
+++ src/main/java/Graph/Node.java
@@ -0,0 +1,7 @@
+package Graph;
+
+/**
+ * Created by matthewlemay on 3/26/16.
+ */
+public class Node {
+}
---
+++ src/main/java/HospitalGraph/Destination.java
@@ -0,0 +1,12 @@
+package HospitalGraph;
+
+/**
+ * Created by matthewlemay on 3/26/16.
+ */
+public enum Destination {
+
+    Physician,
+    Bathroom,
+    Kiosk,
+
+}
