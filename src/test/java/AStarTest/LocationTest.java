9460ded1c5c0c2fe3605cd0a51d2678efc7ac054
Matt <mplemay97@gmail.com>
2016-03-27 15:49:06 -0400

    Added  test cases for AStar.Location class

---
+++ src/test/java/AStarTest/LocationTest.java
@@ -0,0 +1,34 @@
+package AStarTest;
+
+import AStar.Location;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Matt on 3/27/2016.
+ */
+public class LocationTest {
+
+
+    @Test
+    public void locationInitialized() {
+        Location testLocation = new Location(1, 2);
+
+        assertEquals(testLocation.getClass().toString(), "class AStar.Location");
+    }
+
+    @Test
+    public void locationGetX() {
+        Location testLocation = new Location(200, 300);
+
+        assertEquals(testLocation.getX(), 200);
+    }
+
+    @Test
+    public void locationGetY() {
+        Location testLocation = new Location(123, 456);
+
+        assertEquals(testLocation.getY(), 456);
+    }
+}
---
+++ src/test/java/AStarTest/MapTest.java
@@ -0,0 +1,19 @@
+package AStarTest;
+
+import AStar.Map;
+import org.junit.Test;
+
+/**
+ * Created by Matt on 3/26/2016.
+ */
+public class MapTest {
+
+
+    private final String pathToGraph = "";
+    private Map map = new Map(pathToGraph);
+
+    @Test
+    public void m() {
+
+    }
+}
