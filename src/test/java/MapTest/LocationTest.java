a7a8feee9842e63c9ebf0003e7ab718aec915ad9
mplemay <mplemay97@gmail.com>
2016-03-31 18:17:07 -0400

    Removed Kiosk Package

--- src/test/java/MapTest/LocationTest.java
+++ src/test/java/MapTest/LocationTest.java
@@ -1,6 +1,7 @@
-package AStarTest;
+package MapTest;
 
 import Map.Location;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -13,22 +14,50 @@ public class LocationTest {
 
     @Test
     public void locationInitialized() {
+
         Location testLocation = new Location(1, 2);
 
         assertEquals(testLocation.getClass().toString(), "class Map.Location");
+
     }
 
     @Test
     public void locationGetX() {
+
         Location testLocation = new Location(200, 300);
 
         assertEquals(testLocation.getX(), 200);
+
     }
 
     @Test
     public void locationGetY() {
+
         Location testLocation = new Location(123, 456);
 
         assertEquals(testLocation.getY(), 456);
+
+    }
+
+    @Test
+    public void locationSetX() {
+
+        Location testLocation = new Location(333, 444);
+
+        testLocation.setX(6543);
+
+        assertEquals(testLocation.getX(), 6543);
+
+    }
+
+    @Test
+    public void locationSetY() {
+
+        Location testLocation = new Location(532, 313);
+
+        testLocation.setY(432);
+
+        assertEquals(testLocation.getY(), 432);
+
     }
 }
