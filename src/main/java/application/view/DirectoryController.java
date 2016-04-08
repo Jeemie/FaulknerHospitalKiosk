91010d50331e94fe4d0a77fc5c859774374e0d79
rrcooper467 <rrcooper467@gmail.com>
2016-04-07 20:26:25 -0400

    Updated directoryview to contail list view and added some unimplemented lines in the directorycontroller

--- src/main/java/application/view/DirectoryController.java
+++ src/main/java/application/view/DirectoryController.java
@@ -1,7 +1,10 @@
 package application.view;
 
-import javafx.fxml.FXML;
 import application.MainApp;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 
 public class DirectoryController {
@@ -10,90 +13,105 @@ public class DirectoryController {
     // Reference to the main application.
     private MainApp mainApp;
 
+    @FXML
+    private ListView<String> listDirectory;
+
     /**
      * Initializes the controller class. This method is automatically called
      * after the fxml file has been loaded.
      */
     @FXML
     private void initialize() {
+
+        //.sort comparator figure it out form there
+        // comparator uses a generic class so you have to use <>
+
+        ObservableList<String> names = FXCollections.observableArrayList(
+                      "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise");
+        listDirectory.setItems(names);
+
+
     }
-    
+
     /**
      * Is called by the main application to give a reference back to itself.
-     * 
+     *
      * @param mainApp
      */
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
     }
-    
+
     /**
      * Returns true if the user clicked OK, false otherwise.
-     * 
+     *
      * @return
      */
     public boolean isOkClicked() {
         return okClicked;
     }
-    
+
     /**
      * Called when the user clicks enter in the search bar.
      */
     @FXML
     private void handleSearch() {
-    	// TODO: showSearch should have parameter for the input
-    	mainApp.showSearch();
+        // TODO: showSearch should have parameter for the input
+        mainApp.showSearch();
     }
-    
+
     /**
      * Called when the user clicks the Physicians button.
      */
     @FXML
     private void handlePhysicians() {
-    	// TODO: showDirectory should have parameter for category
-    	mainApp.showDirectory();
+        // TODO: showDirectory should have parameter for category
+        mainApp.showDirectory();
     }
-    
-    /** 
+
+    /**
      * Called when the user clicks the Departments button.
      */
     @FXML
     private void handleDepartments() {
-    	// TODO: showDirectory should have parameter for category
-    	mainApp.showDirectory();
+        // TODO: showDirectory should have parameter for category
+        mainApp.showDirectory();
     }
-    
+
     /**
      * Called when the user clicks the Services button.
      */
     @FXML
     private void handleServices() {
-    	// TODO: showDirectory should have parameter for category
-    	mainApp.showDirectory();
+        // TODO: showDirectory should have parameter for category
+        mainApp.showDirectory();
     }
-    
+
     /**
      * Called when the user clicks back.
      */
     @FXML
     private void handleBack() {
-    	mainApp.reset();
+        mainApp.reset();
     }
-    
+
     /**
      * Called when the user clicks cancel.
      */
     @FXML
     private void handleCancel() {
-    	this.handleBack();
+        this.handleBack();
     }
-    
+
     /**
      * Called when the user clicks Forward.
      */
     @FXML
     private void handleForward() {
-    	mainApp.showMap();
+        //listDirectory.getSelectionModel().getSelectedItem();
+        mainApp.showMap();
     }
 
- }
+
+
+}
