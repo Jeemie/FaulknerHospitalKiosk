8990d7ffb3052a32f1101a1c85680b6d9adc6ed0
Matthew LeMay <mplemay97@gmail.com>
2016-04-12 18:12:18 -0400

    Adding Ids to ui elements

--- src/main/java/Kiosk/Controllers/AdminDashboardController.java
+++ src/main/java/Kiosk/Controllers/AdminDashboardController.java
@@ -2,9 +2,7 @@ package Kiosk.Controllers;
 
 import Map.Building;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.StackPane;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,7 +34,129 @@ public class AdminDashboardController {
     @FXML
     private TabPane mapTabPane;
 
-    
+
+
+    //\\ Building Tab //\\
+    @FXML
+    private Tab buildingTab;
+
+    @FXML
+    private Accordion buildingAccordion;
+
+
+    // Floors Titled Pane //
+    @FXML
+    private TitledPane buildingFloorsTitledPane;
+
+    @FXML
+    private ListView buildingFloorsListView;
+
+    @FXML
+    private Button buildingFloorsAddButton;
+
+    @FXML
+    private Button buildingFloorsModifyButton;
+
+    @FXML
+    private Button buildingFloorsDeleteButton;
+
+
+    // Destinations Titled Pane //
+    @FXML
+    private TitledPane buildingDestinationsTitledPane;
+
+    @FXML
+    private ListView buildingDestinationsListView;
+
+
+    // Destinations Titled Pane //
+    @FXML
+    private TitledPane buildingInformationTitledPane;
+
+
+
+    //\\ Floor Tab //\\
+    @FXML
+    private Tab floorTab;
+
+    @FXML
+    private Accordion floorAccordion;
+
+
+    // Locations Titled Pane //
+    @FXML
+    private TitledPane floorLocationsTitledPane;
+
+    @FXML
+    private ListView floorLocationsListView;
+
+    @FXML
+    private Button floorLocationsAddButton;
+
+    @FXML
+    private Button floorLocationsModifyButton;
+
+    @FXML
+    private Button floorLocationsDeleteButton;
+
+
+    // Floor Destinations Titled Pane //
+    @FXML
+    private TitledPane floorDestinationsTitledPane;
+
+    @FXML
+    private ListView floorDestinationsListView;
+
+
+    // Floor Information Titled Pane //
+    @FXML
+    private TitledPane floorInformationTitledPane;
+
+
+
+    //\\ Location Tab //\\
+    @FXML
+    private Tab locationTab;
+
+    @FXML
+    private Accordion locationAccordion;
+
+
+    // Connected Locations Titled Pane //
+    @FXML
+    private TitledPane locationConnectedLocationsTitledPane;
+
+    @FXML
+    private TableView locationConnectedLocationsTableView;
+
+    @FXML
+    private TableColumn locationConnecteLocationsLocationTableColumn;
+
+    @FXML
+    private TableColumn locationConnecteLocationsFloorTableColumn;
+
+    @FXML
+    private Button locationConnectedLocationsAddButton;
+
+    @FXML
+    private Button locationConnectedLocationsDeleteButton;
+
+
+    // Destinations Titled Pane //
+    @FXML
+    private TitledPane
+
+    @FXML
+    private
+
+    @FXML
+    private
+
+    @FXML
+    private
+
+    @FXML
+    private
 
 
 
