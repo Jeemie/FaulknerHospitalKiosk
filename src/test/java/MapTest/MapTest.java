85eb1d26fc10f5c3a1b764f6fd3fe0cb301e360b
binamkayastha <binamkayastha@gmail.com>
2016-04-15 08:10:15 -0400

    Added MapTest

--- src/test/java/MapTest/MapTest.java
+++ src/test/java/MapTest/MapTest.java
@@ -1,9 +1,62 @@
 package MapTest;
 
+import Map.Building;
+import Map.Floor;
+import Map.Map;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by maryannoconnell on 4/12/16.
  */
 public class MapTest {
+
+    private Map mMap = new Map();
+    private Building mTestBuilding;
+    private Floor mFloor;
+
+    @Before
+    public void setup() {
+        mTestBuilding = new Building();
+        mFloor = new Floor (1, mTestBuilding, "picName");
+    }
+
+    @Test
+    public void testInitMapComponentsEmptyBuilding() {
+        Exception ex = null;
+        try {
+            mMap.initMapComponents(mTestBuilding);
+        } catch (Exception e) {
+            ex = e;
+        }
+        Assert.assertEquals(null, ex);
+    }
+
+    @Test
+    public void testInitMapComponents() {
+        mTestBuilding.addFloor(1, "picName");
+        Exception ex = null;
+        try {
+            mMap.initMapComponents(mTestBuilding);
+        } catch (Exception e) {
+        ex = e;
+        }
+        Assert.assertEquals(null, ex);
+    }
+
+    @Test
+    public void testEmptyGetCurrentFloor() {
+        Assert.assertEquals(null, mMap.getNodeFloor());
+    }
+
+    @Test
+    public void testSetAndGetCurrentFloor() {
+        mMap.setCurrentFloor(mFloor);
+
+        Assert.assertEquals(1, mMap.getNodeFloor().getFloor());
+    }
+
 
 
 
