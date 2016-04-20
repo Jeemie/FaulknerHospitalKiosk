e79446e510020f5a1a0aafa1120272ad8085e9b8
Matthew LeMay <mplemay97@gmail.com>
2016-04-19 23:39:33 -0400

    Simplified methods

--- src/main/java/Map/Enums/CardinalDirection.java
+++ src/main/java/Map/Enums/CardinalDirection.java
@@ -7,58 +7,26 @@ package Map.Enums;
 public enum CardinalDirection {
 
     NORTH,
-    SOUTH,
     EAST,
-    WEST,
-    ERR;
+    SOUTH,
+    WEST;
 
     //Get the opposites of the compas, used by the getTextualDirections function
     public CardinalDirection opposite() {
-        switch(this) {
-            case NORTH:
-                return CardinalDirection.SOUTH;
-            case EAST:
-                return CardinalDirection.WEST;
-            case SOUTH:
-                return CardinalDirection.NORTH;
-            case WEST:
-                return CardinalDirection.EAST;
-            default:
-                return CardinalDirection.ERR;
-        }
+
+        return CardinalDirection.values()[(this.ordinal() + 2) % 4];
     }
 
     //Get the value to the right of the compass, used by getTextualDirections function
     public CardinalDirection right() {
-        switch(this) {
-            case NORTH:
-                return CardinalDirection.EAST;
-            case EAST:
-                return CardinalDirection.SOUTH;
-            case SOUTH:
-                return CardinalDirection.WEST;
-            case WEST:
-                return CardinalDirection.NORTH;
-            default:
-                return CardinalDirection.ERR;
-        }
 
+        return CardinalDirection.values()[(this.ordinal() + 1) % 4];
     }
 
     //Get the value of the left of the compass, used by getTextualDirections function
     public CardinalDirection left() {
-        switch(this) {
-            case NORTH:
-                return CardinalDirection.WEST;
-            case EAST:
-                return CardinalDirection.NORTH;
-            case SOUTH:
-                return CardinalDirection.EAST;
-            case WEST:
-                return CardinalDirection.SOUTH;
-            default:
-                return CardinalDirection.ERR;
-        }
+
+        return CardinalDirection.values()[(this.ordinal() + 3) % 4];
     }
 
 }
