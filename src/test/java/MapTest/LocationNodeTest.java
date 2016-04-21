ded7fd4f0f6123d0750826e2bee9bdae795969f7
binamkayastha <binamkayastha@gmail.com>
2016-04-20 21:36:15 -0400

    Added lots of Location Node testing

--- src/test/java/MapTest/LocationNodeTest.java
+++ src/test/java/MapTest/LocationNodeTest.java
@@ -1,10 +1,13 @@
 package MapTest;
 
 import Map.*;
+import Map.Enums.DestinationType;
+import Map.Enums.ImageType;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -17,45 +20,53 @@ public class LocationNodeTest {
      */
     private LocationNode mNode3A, mNode3B, mNode3C, mNode3D;
 
+
     @Before
     public void setUp() throws Exception {
 
         Building mMainHospital = new Building();
-        Floor mFloor3 = new Floor(3, mMainHospital, "Floor3_Final.png");
+
+        Floor mFloor3 = new Floor("Floor 3", ImageType.FLOOR, mMainHospital);
+
         Location mLocation3B = new Location(10, 10);
-        mNode3A = new LocationNode(0, mLocation3B, mFloor3);
-        mNode3B = new LocationNode(0, new Location (10, 15), mFloor3);
-        mNode3C = new LocationNode(0, new Location(10, 30), mFloor3);
-        mNode3D = new LocationNode(0, new Location(10, 40), mFloor3);
+
+        mNode3A = new LocationNode("Hallway", mLocation3B, mFloor3, ImageType.POINT);
+        mNode3B = new LocationNode("Hallway", new Location (10, 15), mFloor3, ImageType.POINT);
+        mNode3C = new LocationNode("Hallway", new Location(10, 30), mFloor3, ImageType.POINT);
+        mNode3D = new LocationNode("Hallway", new Location(10, 40), mFloor3, ImageType.POINT);
+
     }
 
     /**
      * Add new department destination to specified location
+     * Also tests getDestinations
      */
     @Test
     public void testAddDestinationDepartment() {
-        mNode3A.addDestination(Map.DestinationType.DEPARTMENT, "Optometry");
-        ArrayList<String> destinations = mNode3A.getDestinations(Map.DestinationType.DEPARTMENT);
+        Destination destination = new Destination ("Optometry", DestinationType.DEPARTMENT, mNode3A);
+        ArrayList<Destination> destinations = mNode3A.getDestinations(DestinationType.DEPARTMENT);
         Assert.assertEquals(destinations.contains("Optometry"), true);
     }
 
     /**
      * Add new physician destination to specified location
+     * Also tests getDestinations
      */
     @Test
     public void testAddDestinationPhysician() {
-        mNode3B.addDestination(Map.DestinationType.PHYSICIAN, "Dr. Lisa Grossi");
-        ArrayList<String> destinations = mNode3B.getDestinations(Map.DestinationType.PHYSICIAN);
+        Destination destination = new Destination ("Dr. Lisa Grossi", DestinationType.PHYSICIAN, mNode3B);
+        ArrayList<Destination> destinations = mNode3B.getDestinations(DestinationType.PHYSICIAN);
         Assert.assertEquals(destinations.contains("Dr. Lisa Grossi"), true);
     }
 
     /**
      * Add new elevator destination to specified location
+     * Also tests getDestinations
      */
     @Test
     public void testAddDestinationElevator() {
-        mNode3C.addDestination(Map.DestinationType.ELEVATOR, "Elevator");
-        ArrayList<String> destinations = mNode3C.getDestinations(Map.DestinationType.ELEVATOR);
+        Destination destination = new Destination ("Elevator", DestinationType.ELEVATOR, mNode3C);
+        ArrayList<Destination> destinations = mNode3C.getDestinations(DestinationType.ELEVATOR);
         Assert.assertEquals(destinations.contains("Elevator"), true);
     }
 
@@ -64,48 +75,49 @@ public class LocationNodeTest {
      */
     @Test
     public void testAddRemoveDestinationKiosk() {
-        mNode3D.addDestination(Map.DestinationType.KIOSK, "Kiosk");
-        ArrayList<String> destinations = mNode3D.getDestinations(Map.DestinationType.KIOSK);
+        Destination destination = new Destination ("Kiosk", DestinationType.KIOSK, mNode3D);
+        ArrayList<Destination> destinations = mNode3D.getDestinations(DestinationType.KIOSK);
         Assert.assertEquals(destinations.contains("Kiosk"), true);
-        mNode3D.removeDestination(Map.DestinationType.KIOSK, "Kiosk");
-        destinations = mNode3D.getDestinations(Map.DestinationType.KIOSK);
+        mNode3D.removeDestination(destination);
+        destinations = mNode3D.getDestinations(DestinationType.KIOSK);
         Assert.assertEquals(destinations.contains("Kiosk"), false);
     }
 
     /**
      * Add adjacent nodes
      */
-    @Test
-    public void testAddAdjacentNodes() {
-        mNode3A.addAdjacentNode(mNode3B);
-        mNode3A.addAdjacentNode(mNode3C);
-        mNode3A.addAdjacentNode(mNode3D);
-        ArrayList<LocationNode> adjEyeCareNodes = mNode3A.getAdjacentLocationNodes();
-        ArrayList<LocationNode> expectedVals = new ArrayList<>();
-        expectedVals.add(mNode3B);
-        expectedVals.add(mNode3C);
-        expectedVals.add(mNode3D);
-        Assert.assertEquals(expectedVals, adjEyeCareNodes);
-    }
+    //Does not exist yet
+//    @Test
+//    public void testAddAdjacentNodes() {
+//        mNode3A.addAdjacentNode(mNode3B);
+//        mNode3A.addAdjacentNode(mNode3C);
+//        mNode3A.addAdjacentNode(mNode3D);
+//        ArrayList<LocationNode> adjEyeCareNodes = mNode3A.getAdjacentLocationNodes();
+//        ArrayList<LocationNode> expectedVals = new ArrayList<>();
+//        expectedVals.add(mNode3B);
+//        expectedVals.add(mNode3C);
+//        expectedVals.add(mNode3D);
+//        Assert.assertEquals(expectedVals, adjEyeCareNodes);
+//    }
 
     /**
      * Remove an adjacent node
      */
-    @Test
-    public void testRemoveAdjacentNode() {
-        mNode3A.addAdjacentNode(mNode3B);
-        mNode3A.addAdjacentNode(mNode3C);
-        mNode3A.addAdjacentNode(mNode3D);
-        ArrayList<LocationNode> adjEyeCareNodes = mNode3A.getAdjacentLocationNodes();
-        ArrayList<LocationNode> expectedVals = new ArrayList<>();
-        expectedVals.add(mNode3B);
-        expectedVals.add(mNode3C);
-        expectedVals.add(mNode3D);
-        Assert.assertEquals(expectedVals, adjEyeCareNodes);
-        mNode3A.removeAdjacentNode(mNode3C);
-        expectedVals.remove(mNode3C);
-        Assert.assertEquals(expectedVals, adjEyeCareNodes);
-    }
+//    @Test
+//    public void testRemoveAdjacentNode() {
+//        mNode3A.addAdjacentNode(mNode3B);
+//        mNode3A.addAdjacentNode(mNode3C);
+//        mNode3A.addAdjacentNode(mNode3D);
+//        ArrayList<LocationNode> adjEyeCareNodes = mNode3A.getAdjacentLocationNodes();
+//        ArrayList<LocationNode> expectedVals = new ArrayList<>();
+//        expectedVals.add(mNode3B);
+//        expectedVals.add(mNode3C);
+//        expectedVals.add(mNode3D);
+//        Assert.assertEquals(expectedVals, adjEyeCareNodes);
+//        mNode3A.removeAdjacentNode(mNode3C);
+//        expectedVals.remove(mNode3C);
+//        Assert.assertEquals(expectedVals, adjEyeCareNodes);
+//    }
 
     /**
      * Get distance between two nodes
@@ -113,6 +125,97 @@ public class LocationNodeTest {
     @Test
     public void testGetDistanceBetween() {
         double distance = mNode3A.getDistanceBetweenNodes(mNode3B);
-        Assert.assertEquals(distance, 5.0, 0.001);
+        Assert.assertEquals(5.0, distance, 0.001);
+    }
+
+    /**
+     * Tests to make sure it fails
+     */
+    @Test
+    public void testDrawAdminNULL() {
+        Exception ex = null;
+        try {
+            mNode3A.drawAdmin(null);
+        } catch (NullPointerException e) {
+           ex = e;
+        }
+        Assert.assertEquals(new NullPointerException(), ex);
+    }
+
+    /**
+     * DeleteNode
+     */
+    @Test
+    public void testDeleteNode() {
+
+        mNode3A.deleteNode();
+        Assert.assertEquals(null, mNode3A);
+
+    }
+
+    /**
+     * Test to make sure that add edge actually adds an edge
+     */
+    @Test
+    public void testAddEdge() {
+        boolean containsEdgeToB = false;
+        boolean containsEdgeToA = false;
+        mNode3A.addEdge(mNode3B);
+
+        for (LocationNodeEdge e: mNode3A.getEdges()) {
+            if(e.getLocationNode1().equals(mNode3B) || e.getLocationNode2().equals(mNode3B)) {
+                containsEdgeToB = true;
+            }
+        }
+
+        for (LocationNodeEdge e: mNode3B.getEdges()) {
+            if(e.getLocationNode1().equals(mNode3A) || e.getLocationNode2().equals(mNode3A)) {
+                containsEdgeToA = true;
+            }
+        }
+
+        Assert.assertTrue(containsEdgeToA && containsEdgeToB);
+
+    }
+
+    /**
+     * Test to get the location of the node, and check x and y
+     */
+    @Test
+    public void testGetLocation() {
+        Assert.assertTrue(10 == mNode3A.getLocation().getX());
+        Assert.assertTrue(10 == mNode3A.getLocation().getY());
+    }
+
+    /**
+     * Test to see get edges of a node
+     */
+    @Test
+    public void testGetEdges() {
+
+    }
+
+    /**
+     * Test the toString function
+     */
+    @Test
+    public void testToString() {
+
+    }
+
+    /**
+     * Test the compareTo function
+     */
+    @Test
+    public void testCompareTo() {
+
+    }
+
+    /**
+     * Test if the location actually updates
+     */
+    @Test
+    public void testUpdate() {
+
     }
 }
