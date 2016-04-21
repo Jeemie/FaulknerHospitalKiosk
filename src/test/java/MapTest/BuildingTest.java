ba58795676c9a4b820909f31c30f0f71b3693dfd
mjoconnell2 <maryann.oc91@gmail.com>
2016-04-21 02:53:58 -0400

    Update BuildingTest class methods

--- src/test/java/MapTest/BuildingTest.java
+++ src/test/java/MapTest/BuildingTest.java
@@ -16,7 +16,7 @@ public class BuildingTest {
     @Before
     public void setUp() {
 
-        mTestBuilding = new Building();
+        mTestBuilding = new Building("test building", new Map("test map"));
     }
 
     /**
@@ -37,6 +37,8 @@ public class BuildingTest {
     @Test
     public void testAddMultipleFloors() {
 
+        Assert.assertTrue(mTestBuilding.getFloors().size() == 0);
+
         mTestBuilding.addFloor("Floor 1", ImageType.FLOOR);
         mTestBuilding.addFloor("Floor 2", ImageType.FLOOR);
         mTestBuilding.addFloor("Floor 3", ImageType.FLOOR);
@@ -46,19 +48,41 @@ public class BuildingTest {
     }
 
     /**
+     * Remove floor from building
+     */
+    @Test
+    public void testRemoveFloor() {
+
+        mTestBuilding.addFloor("Floor 1", ImageType.FLOOR);
+        mTestBuilding.addFloor("Floor 2", ImageType.FLOOR);
+        mTestBuilding.addFloor("Floor 3", ImageType.FLOOR);
+
+        Floor oldFloor = mTestBuilding.getFloors().get(0);
+
+        mTestBuilding.removeFloor(oldFloor);
+
+
+        Assert.assertFalse(mTestBuilding.getFloors().contains(oldFloor));
+
+    }
+
+
+
+    /**
      * Add node to building
      */
-   /* @Test
+  /*  @Test
     public void testAddNode() throws FloorDoesNotExistException {
 
         mTestBuilding.addFloor(1, "Floor1_Final.png");
-        mTestBuilding.addNode(1, new Location(100, 100));
+        mTestBuilding.addLocationNode(1, new Location(100, 100));
 
         Assert.assertTrue(mTestBuilding.getFloor(1).getLocationNodes().size() == 1);
 
     }
+    */
 
-    *//**
+    /**
      * Get service destinations from floor one
      *//*
 
