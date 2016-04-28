f68fb1f6f9ade1633634647c0e803c9386875618
maryannoconnell <maryann.oc91@gmail.com>
2016-04-27 20:34:32 -0400

    Added LocationNodeTest methods then tested them. Note they are now commented out because methods with JavaFX atributes and methods can't be called by JUnit. Fixed error caused by iterator when attempting to removing all edges of a node.

--- src/test/java/MapTest/LocationNodeTest.java
+++ src/test/java/MapTest/LocationNodeTest.java
@@ -1,194 +1,221 @@
-package MapTest;
-
-import Map.Building;
-import Map.Floor;
-import Map.LocationNode;
-import Map.LocationNodeEdge;
-import Map.Location;
-import Map.Destination;
-import Map.Enums.DestinationType;
-import Map.Enums.ImageType;
-import Map.Exceptions.NodeDoesNotExistException;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.ArrayList;
-
-/**
- * Created by maryannoconnell on 4/3/16.
- */
-public class LocationNodeTest {
-
-    /**
-     * LocationNodes for testing
-     */
-    private LocationNode mNode3A, mNode3B, mNode3C, mNode3D;
-
+//package MapTest;
+//
+//
+//import Map.Map;
+//import Map.LocationNode;
+//import Map.Location;
+//import Map.Destination;
+//import Map.LocationNodeEdge;
+//import Map.Enums.DestinationType;
+//import Map.Enums.ImageType;
+//import Map.Exceptions.NodeDoesNotExistException;
+//import org.junit.Assert;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import java.util.ArrayList;
+//
+///**
+// * Created by maryannoconnell on 4/3/16.
+// */
+//public class LocationNodeTest {
+//
+//    /**
+//     * Map for testing
+//     */
+//    Map mTestMap;
+//
+//    /**
+//     * LocationNodes for testing
+//     */
+//    LocationNode mNode3A, mNode3B, mNode3C, mNode3D;
+//
 //    @Before
 //    public void setUp() throws Exception {
 //
-//        Building mMainHospital = new Building();
-//        Floor mFloor3 = new Floor("Floor 3", "Floor1_Final.png", mMainHospital);
-//        Location mLocation3B = new Location(10, 10);
+//        mTestMap = new Map("test map");
 //
-//        mNode3A = new LocationNode("3A", mLocation3B, mFloor3, ImageType.POINT);
-//        mNode3B = new LocationNode("3B", new Location (10, 15), mFloor3, ImageType.POINT);
-//        mNode3C = new LocationNode("3C", new Location(10, 30), mFloor3, ImageType.POINT);
-//        mNode3D = new LocationNode("3D", new Location(10, 40), mFloor3, ImageType.POINT);
+//        mTestMap.addBuilding("test building");
+//        mTestMap.addFloor("Floor 3", "floor1.png");
+//
+//
+//        // Set current floor before adding nodes
+//        mTestMap.setCurrentFloor(mTestMap.getMapBuildings().get(0).getFloors().get(0));
+//
+//        // 3A - index 0
+//        mTestMap.addLocationNode("3A", new Location(100,100), ImageType.WAITINGROOM);
+//        // 3B - index 1
+//        mTestMap.addLocationNode("3B", new Location(100,150), ImageType.WAITINGROOM);
+//        // 3C - index 2
+//        mTestMap.addLocationNode("3C", new Location(100,300), ImageType.WAITINGROOM);
+//        // 3D - index 3
+//        mTestMap.addLocationNode("3D", new Location(150,100), ImageType.WAITINGROOM);
+//
+//        mNode3A = mTestMap.getMapBuildings().get(0).getFloors().get(0).getFloorNodes().get(0);
+//        mNode3B = mTestMap.getMapBuildings().get(0).getFloors().get(0).getFloorNodes().get(1);
+//        mNode3C = mTestMap.getMapBuildings().get(0).getFloors().get(0).getFloorNodes().get(2);
+//        mNode3D = mTestMap.getMapBuildings().get(0).getFloors().get(0).getFloorNodes().get(3);
 //    }
-
-    /**
-     * Add new department destination to specified location
-     */
-    @Test
-    public void testAddDestinationDepartment() {
-
-        mNode3A.addDestination("Optometry", DestinationType.DEPARTMENT);
-        ArrayList<Destination> destinations = mNode3A.getDestinations(DestinationType.DEPARTMENT);
-
-        Assert.assertEquals(destinations.get(0).toString(), "Optometry");
-    }
-
-    /**
-     * Add new physician destination to specified location
-     */
-  @Test
-    public void testAddDestinationPhysician() {
-
-        mNode3B.addDestination("Dr. Lisa Grossi", DestinationType.PHYSICIAN);
-        ArrayList<Destination> destinations = mNode3B.getDestinations(DestinationType.PHYSICIAN);
-
-      Assert.assertEquals(destinations.get(0).toString(), "Dr. Lisa Grossi");
-    }
-
-    /**
-     * Add new elevator destination to specified location
-     */
-    @Test
-    public void testAddDestinationElevator() {
-
-        mNode3C.addDestination("Elevator", DestinationType.ELEVATOR);
-        ArrayList<Destination> destinations = mNode3C.getDestinations(DestinationType.ELEVATOR);
-
-        Assert.assertEquals(destinations.get(0).toString(), "Elevator");
-    }
-
-    /**
-     * Add new kiosk destination to specified location, then remove kiosk destination
-     */
-    @Test
-    public void testAddRemoveDestinationKiosk()  throws NodeDoesNotExistException  {
-
-        mNode3D.addDestination("Kiosk", DestinationType.KIOSK);
-        ArrayList<Destination> destinations = mNode3D.getDestinations(DestinationType.KIOSK);
-        Assert.assertEquals(destinations.get(0).toString(), "Kiosk");
-        mNode3D.removeDestination(destinations.get(0));
-        destinations = mNode3D.getDestinations(DestinationType.KIOSK);
-        Assert.assertTrue(destinations.isEmpty());
-    }
-
-    /**
-     * Add adjacent nodes
-     */
-    @Test
-    public void testAddAdjacentNodes() throws NodeDoesNotExistException {
-        mNode3A.addEdge(mNode3B);
-        mNode3A.addEdge(mNode3C);
-        mNode3A.addEdge(mNode3D);
-        ArrayList<LocationNode> adjEyeCareNodes = mNode3A.getAdjacentLocationNodes();
-        ArrayList<LocationNode> expectedVals = new ArrayList<>();
-        expectedVals.add(mNode3B);
-        expectedVals.add(mNode3C);
-        expectedVals.add(mNode3D);
-        Assert.assertEquals(expectedVals, adjEyeCareNodes);
-    }
-
-    /**
-     * Remove an edge connecting an adjacent node
-     */
-    @Test
-    public void testRemoveEdgeConnection() throws NodeDoesNotExistException {
-
-        // Add edge connections to node
-        mNode3A.addEdge(mNode3B);
-        mNode3A.addEdge(mNode3C);
-        mNode3A.addEdge(mNode3D);
-
-        // Store adjacent nodes in list
-        ArrayList<LocationNode> adjEyeCareNodes = mNode3A.getAdjacentLocationNodes();
-
-        // Create list for expected values
-        ArrayList<LocationNode> expectedVals = new ArrayList<>();
-
-        // Add nodes expected to be in list
-        expectedVals.add(mNode3B);
-        expectedVals.add(mNode3C);
-        expectedVals.add(mNode3D);
-
-        // Verify that actual values and expected values are equal
-        Assert.assertEquals(expectedVals, adjEyeCareNodes);
-
-        // Get the edges of mNode3A
-        ArrayList<LocationNodeEdge> edges = mNode3A.getEdges();
-
-        // Get the edge from the list of edges that connects mNode3A and mNode3B
-        LocationNodeEdge mEdge1 = LocationNodeEdge.getEdgeBetween(edges, mNode3A, mNode3B);
-
-        // Remove the edge connecting mNode3A and mNode3B
-        mNode3A.removeEdgeConnection(mEdge1);
-
-        // Get updated list of adjacent nodes
-        adjEyeCareNodes = mNode3A.getAdjacentLocationNodes();
-
-        // mNode3B should no longer be in list
-        expectedVals.remove(mNode3B);
-
-        Assert.assertEquals(expectedVals, adjEyeCareNodes);
-    }
-
-    /**
-     * Get distance between two nodes
-     */
-    @Test
-    public void testGetDistanceBetween() {
-
-        double distance = mNode3A.getDistanceBetweenNodes(mNode3B);
-        Assert.assertEquals(distance, 5.0, 0.001);
-
-    }
-
-    /**
-     * Delete all edges connected to a node
-     */
-    @Test
-    public void testDeleteLocationNodeEdgeConnections() throws NodeDoesNotExistException {
-
-        // Add edge connections to node
-        mNode3A.addEdge(mNode3B);
-        mNode3A.addEdge(mNode3C);
-        mNode3A.addEdge(mNode3D);
-
-        // Store adjacent nodes in list
-        ArrayList<LocationNode> adjEyeCareNodes = mNode3A.getAdjacentLocationNodes();
-
-        // Create list for expected values
-        ArrayList<LocationNode> expectedVals = new ArrayList<>();
-
-        // Add nodes expected to be in list
-        expectedVals.add(mNode3B);
-        expectedVals.add(mNode3C);
-        expectedVals.add(mNode3D);
-
-        // Verify that actual values and expected values are equal
-        Assert.assertEquals(expectedVals, adjEyeCareNodes);
-
-        mNode3A.deleteLocationNodeEdgeConnections();
-
-        // Get the updated list edges of mNode3A (expected to be empty)
-        ArrayList<LocationNodeEdge> edges = mNode3A.getEdges();
-
-        Assert.assertTrue(edges.isEmpty());
-    }
-
-}
+//
+//    /**
+//     * Add new department destination to specified location
+//     */
+//
+//    @Test
+//    public void testAddDestinationDepartment() {
+//
+//
+//        mTestMap.addDestination("Optometry", DestinationType.DEPARTMENT);
+//        ArrayList<Destination> destinations = mTestMap.getCurrentLocationNode().getDestinations(DestinationType.DEPARTMENT);
+//
+//        Assert.assertEquals(destinations.get(0).toString(), "Optometry");
+//    }
+//
+//    /**
+//     * Add new kiosk destination to specified location
+//     */
+//    @Test
+//    public void testAddDestinationKiosk()  {
+//
+//        // Set current node before adding destination
+//        mTestMap.setCurrentLocationNode(mNode3D);
+//        mTestMap.addDestination("Kiosk", DestinationType.KIOSK);
+//        ArrayList<Destination> destinations = mTestMap.getCurrentLocationNode().getDestinations(DestinationType.KIOSK);
+//
+//        Assert.assertEquals(destinations.get(0).toString(), "Kiosk");
+//    }
+//
+//    /**
+//     * Add new destination then remove that destination
+//     */
+//    @Test
+//    public void testAddRemoveDestinationKiosk()  throws NodeDoesNotExistException  {
+//
+//        // Set current node before adding destination
+//        mTestMap.setCurrentLocationNode(mNode3D);
+//        mTestMap.addDestination("Kiosk", DestinationType.KIOSK);
+//        ArrayList<Destination> destinations = mTestMap.getCurrentLocationNode().getDestinations(DestinationType.KIOSK);
+//
+//        Assert.assertEquals(destinations.get(0).toString(), "Kiosk");
+//
+//        mTestMap.getCurrentLocationNode().removeDestination(destinations.get(0));
+//        destinations = mTestMap.getCurrentLocationNode().getDestinations(DestinationType.KIOSK);
+//        Assert.assertTrue(destinations.isEmpty());
+//    }
+//
+//    /**
+//     * Add edges using method in Map class and get adjacent nodes
+//     */
+//    @Test
+//    public void testGetAdjacentNodes() throws NodeDoesNotExistException {
+//
+//        // Set current location node
+//        mTestMap.setCurrentLocationNode(mNode3A);
+//
+//        // Add 3 edges to current location node using method from Map class
+//        mTestMap.addLocationNodeEdge(mNode3B);
+//        mTestMap.addLocationNodeEdge(mNode3C);
+//        mTestMap.addLocationNodeEdge(mNode3D);
+//
+//        ArrayList<LocationNode> adjEyeCareNodes = mNode3A.getAdjacentLocationNodes();
+//        ArrayList<LocationNode> expectedVals = new ArrayList<>();
+//
+//        expectedVals.add(mNode3B);
+//        expectedVals.add(mNode3C);
+//        expectedVals.add(mNode3D);
+//
+//        Assert.assertEquals(expectedVals, adjEyeCareNodes);
+//    }
+//
+//    /**
+//     * Remove an edge
+//     */
+//    @Test
+//    public void testRemoveEdge() throws NodeDoesNotExistException {
+//
+//        // Set current location node
+//        mTestMap.setCurrentLocationNode(mNode3A);
+//
+//        // Add 3 edges to current location node using method from Map class
+//        mTestMap.addLocationNodeEdge(mNode3B);
+//        mTestMap.addLocationNodeEdge(mNode3C);
+//        mTestMap.addLocationNodeEdge(mNode3D);
+//
+//        Assert.assertEquals(mNode3A.getEdges().size(), 3);
+//
+//        mNode3A.removeEdgeConnection(mNode3A.getEdges().get(0));
+//
+//        Assert.assertEquals(mNode3A.getEdges().size(), 2);
+//    }
+//
+//    /**
+//     * Remove a node connected to two with edges to two other nodes
+//     * Note: removeLocationNode() is implemented in Map class
+//     */
+//    @Test
+//    public void testRemoveNode() throws NodeDoesNotExistException {
+//
+//        // Set current location node to mNode3B
+//        mTestMap.setCurrentLocationNode(mNode3B);
+//
+//        // Add edge
+//        mTestMap.addLocationNodeEdge(mNode3C);
+//
+//        // Set current location node to mNode3A
+//        mTestMap.setCurrentLocationNode(mNode3A);
+//
+//        // Add 3 edges to current location node using method from Map class
+//        mTestMap.addLocationNodeEdge(mNode3B);
+//        mTestMap.addLocationNodeEdge(mNode3C);
+//        mTestMap.addLocationNodeEdge(mNode3D);
+//
+//        // Remove 3A
+//        mTestMap.removeLocationNode();
+//
+//        // mNode3A (the current location node) is null
+//        Assert.assertEquals(null, mTestMap.getCurrentLocationNode());
+//
+//        Assert.assertEquals(1, mNode3B.getEdges().size());
+//
+//    }
+//
+//    /**
+//     * Get distance between two nodes
+//     */
+//    @Test
+//    public void testGetDistanceBetween() {
+//
+//        double distance = mNode3A.getDistanceBetweenNodes(mNode3B);
+//        Assert.assertEquals(distance, 50.0, 0.001);
+//
+//    }
+//
+//    /**
+//     * Delete all edges connected to a node
+//     */
+//    @Test
+//    public void testDeleteLocationNodeEdgeConnections() throws NodeDoesNotExistException {
+//
+//        LocationNode mNode5;
+//        mTestMap.addLocationNode("node5", new Location(200, 200), ImageType.POINT);
+//        mNode5 = mTestMap.getCurrentFloor().getLocationNodes().get(4);
+//
+//        // Set current location node
+//        mTestMap.setCurrentLocationNode(mNode3A);
+//
+//        // Add 3 edges to current location node using method from Map class
+//        mTestMap.addLocationNodeEdge(mNode3B);
+//        mTestMap.addLocationNodeEdge(mNode3C);
+//        mTestMap.addLocationNodeEdge(mNode3D);
+//        mTestMap.addLocationNodeEdge(mNode5);
+//
+//        Assert.assertEquals(mNode3A.getEdges().size(), 4);
+//
+//        mNode3A.deleteLocationNodeEdgeConnections();
+//
+//        // Get the updated list edges of mNode3A (expected to be empty)
+//        ArrayList<LocationNodeEdge> edges = mNode3A.getEdges();
+//
+//        Assert.assertTrue(edges.isEmpty());
+//    }
+//}
